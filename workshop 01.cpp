eee1f8efaa12ce9633451d20b3e9d644ff7ce1d2
bay-sketch <2500018198@webmail.uad.ac.id>
2026-04-18 23:06:40 +0700

    Add files via upload

---
+++ workshop 01.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+	cout << "===== BIODATA PRIBADI =====\n";
+	cout << "Nama                 : Bayu Riski Patoni\n";
+	cout << "Tempat, Tanggal Lahir: Toban, 27 Desember 2004\n";
+	cout << "Jenis Kelamin        : Laki Laki\n";
+	cout << "Alamat               : Bangunharjo, Bantul\n";
+	cout << "NIM                  : 2500018198\n";
+	cout << "Prodi                : Informatika\n";
+	cout << "Fakultas             : Teknologi Industri\n";
+	return 0;
+}
